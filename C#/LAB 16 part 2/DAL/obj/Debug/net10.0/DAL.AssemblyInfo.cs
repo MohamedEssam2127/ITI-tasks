ad823ae9ed03822cfa744bba49a599012daee41b
MohamedEssam2127 <msam01619@gmail.com>
2026-03-28 20:22:36 +0200

    fix nested repo

--- C#/LAB 16 part 2/DAL/obj/Debug/net10.0/DAL.AssemblyInfo.cs
+++ C#/LAB 16 part 2/DAL/obj/Debug/net10.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483c409c3644a185f0cd81c938f19363183e976b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e322ec781fdf33be09f712aad439247b1e3780")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
